22aad36792260f2e887f8e7025ce340e66cfd178
ingenieriainversa <jdg@member.fsf.org>
2017-01-02 20:22:22 +0100

    Create Main.java

---
+++ src/main/Main.java
@@ -0,0 +1,55 @@
+/*
+ * Logic Calculator v0.01
+ * Main.java
+ * Copyleft - 2017  Javier Dominguez Gomez
+ * Written by Javier Dominguez Gomez <jdg@member.fsf.org>
+ * GnuPG Key: 6ECD1616
+ * Madrid, Spain
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package main;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Main {
+
+	private JFrame frame;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public Main() {
+		initialize();
+	}
+
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 735, 475);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+	}
+}
